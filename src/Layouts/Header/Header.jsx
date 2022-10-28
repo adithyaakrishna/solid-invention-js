a9eedb6b8ee18151714b7b727a6889d9f47f591c
Adithya Krishna <adikrish@redhat.com>
2022-10-29 04:26:44 +0530

    Updated Dummy Function For Button
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -8,13 +8,17 @@ import './Header.min.css'
 import { BasicAlert } from '../../Components/BasicComponents/BasicAlert/BasicAlert';
 
 export const Header = (props) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const onClick = () => {
+    setIsVisible(!isVisible)
+  }
   return (
     
     <Box className="header">
       <AppBar className='header-container' position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1}}>
         <Toolbar className="header">
-          {props.isError ? <BasicAlert iconVisible={false} alertText={"Cannot save flow"} variant="filled" severity="error" /> : <BasicAlert iconVisible={false} alertText={"Flow saved successfully"} variant="filled" severity="success" />}
-          <BasicButton className="save-button" color='inherit' variant="outlined" buttonText='Save Changes' />
+          {props.isError || isVisible ? <BasicAlert iconVisible={false} alertText={"Cannot save flow"} variant="filled" severity="error" /> : <BasicAlert iconVisible={false} alertText={"Flow saved successfully"} variant="filled" severity="success" />}
+          <BasicButton className="save-button" color='inherit' variant="outlined" buttonText='Save Changes' onClick={onClick}/>
         </Toolbar>
       </AppBar>
     </Box>
